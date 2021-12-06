83b41390c2ced78da30ecb29f0031df23e0eaaef
mohammadrezaaliabadi <mohammadrezaaliabadi9@gmail.com>
2021-12-06 19:48:08 +0330

    Manual DI. close #2

---
+++ src/main/java/com/example/sfgdi/controller/PropertyInjectedController.java
@@ -0,0 +1,10 @@
+package com.example.sfgdi.controller;
+
+import com.example.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+    public GreetingService greetingService;
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}

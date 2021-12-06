28783f3924f4e2d67115fc804747c048800e0deb
mohammadrezaaliabadi <mohammadrezaaliabadi9@gmail.com>
2021-12-06 20:02:08 +0330

    Using qualifiers. close #4

--- src/main/java/com/example/sfgdi/controller/PropertyInjectedController.java
+++ src/main/java/com/example/sfgdi/controller/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package com.example.sfgdi.controller;
 
 import com.example.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
+    @Qualifier("propertyInjectedGreetingService")
     public GreetingService greetingService;
     public String getGreeting(){
         return greetingService.sayGreeting();
